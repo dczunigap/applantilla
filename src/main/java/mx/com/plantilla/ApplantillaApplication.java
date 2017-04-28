296b0d7ce6944a0f95b998be004eb50600156611
dczunigap <dczunigap@outlook.com>
2017-04-28 17:54:53 -0500

    - Se agrega configuracion para acceso al REST desde otra ruta virtual /api. - Se agrega la seguridad, pero se deja deshabilitado. Seguridad Basica de Spring Boot - Se configura actuator para monitoreo de la app

--- src/main/java/mx/com/plantilla/ApplantillaApplication.java
+++ src/main/java/mx/com/plantilla/ApplantillaApplication.java
@@ -2,9 +2,16 @@ package mx.com.plantilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApplantillaApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Applantilla World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplantillaApplication.class, args);
