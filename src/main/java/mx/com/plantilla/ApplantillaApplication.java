166216202b9a225fda022403d312290d328ae51d
dczunigap <dczunigap@outlook.com>
2017-05-04 17:17:02 -0500

    - Se implementa seguridad con JWT

--- src/main/java/mx/com/plantilla/ApplantillaApplication.java
+++ src/main/java/mx/com/plantilla/ApplantillaApplication.java
@@ -1,12 +1,16 @@
 package mx.com.plantilla;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
 @SpringBootApplication
 @RestController
+/*@EnableAutoConfiguration*/
 public class ApplantillaApplication {
 	@RequestMapping("/")
 	public String home() {
